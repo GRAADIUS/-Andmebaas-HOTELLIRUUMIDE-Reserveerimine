609a9fed40eaa22e2a6bd8234647434707524e75
Egor Fedorenko <120181389+GRAADIUS@users.noreply.github.com>
2023-09-11 10:53:09 +0300

    Update double_trigger.sql

--- double_trigger.sql
+++ double_trigger.sql
@@ -1,1 +1,26 @@
 -----------------Kasutasin kahte lauda, Guest ja Reservated-----------------------------
+create table guest(
+	guest_ID int primary key identity(1,1),
+	first_name varchar(10),
+	last_name varchar(40) NULL,
+	mumber_since date
+);
+create table reservation(
+	reservation_ID int primary key identity(1,1),
+	date_in date,
+	date_out date,
+	made_by varchar(20),
+	guest_ID int,
+  FOREIGN KEY (guest_ID) REFERENCES guest(guest_ID)
+);
+
+
+create trigger guest_reservation
+on reservation
+for insert
+as
+INSERT INTO logi(kuupaev, kasutaja, andmed)
+select GETDATE(), USER, 
+        concat(inserted.date_in, ', ', inserted.date_out, ', ', inserted.made_by, ', ', m.guest_ID)
+from reservation
+inner join guest g on inserted.guest_ID = m.guest_ID
