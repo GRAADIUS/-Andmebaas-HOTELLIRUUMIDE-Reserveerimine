5f94bf122c11117390ca1d169f5cdff5d181733c
Egor Fedorenko <120181389+GRAADIUS@users.noreply.github.com>
2023-09-11 11:42:56 +0300

    Update double_trigger.sql

--- double_trigger.sql
+++ double_trigger.sql
@@ -1,26 +1,23 @@
------------------Kasutasin kahte lauda, Guest ja Reservated-----------------------------
-create table guest(
-	guest_ID int primary key identity(1,1),
-	first_name varchar(10),
-	last_name varchar(40) NULL,
-	mumber_since date
-);
-create table reservation(
-	reservation_ID int primary key identity(1,1),
-	date_in date,
-	date_out date,
-	made_by varchar(20),
-	guest_ID int,
-  FOREIGN KEY (guest_ID) REFERENCES guest(guest_ID)
-);
+CREATE TRIGGER guest_reservation
+ON reservation
+FOR INSERT
+AS
+INSERT INTO logi(kuupaev, kasutaja, andmed)
+SELECT GETDATE(), USER, 
+    CONCAT(INSERTED.date_in, ', ', INSERTED.date_out, ', ', INSERTED.made_by, ', ', g.first_name, ', ', g.last_name, ', ', g.mumber_since)
+FROM inserted
+INNER JOIN guest g ON INSERTED.guest_ID = g.guest_ID;
 
+----------------------------------------------------------
 
-create trigger guest_reservation
+create trigger reservation_update
 on reservation
-for insert
+for update
 as
 INSERT INTO logi(kuupaev, kasutaja, andmed)
 select GETDATE(), USER, 
-        concat(inserted.date_in, ', ', inserted.date_out, ', ', inserted.made_by, ', ', m.guest_ID)
-from reservation
-inner join guest g on inserted.guest_ID = m.guest_ID
+        concat('Vanad: ', deleted.date_in, ', ', deleted.date_out, ', ', deleted.made_by, ', ', g1.first_name, ', ', g1.last_name, ', ', g1.mumber_since, '. ', 'Uued: ', INSERTED.date_in, ', ', INSERTED.date_out, ', ', INSERTED.made_by, ', ', g2.first_name, ', ', g2.last_name, ', ', g2.mumber_since, '.')
+from deleted
+inner join inserted on deleted.reservation_ID = inserted.reservation_ID
+inner join guest g1 on deleted.guest_ID = g1.guest_ID
+inner join guest g2 on inserted.guest_ID = g2.guest_ID
